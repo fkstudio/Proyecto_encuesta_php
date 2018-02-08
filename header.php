a84fdc8395919821da06d41487a521e9a444b349
Franklyn O. Perez <fkop04@gmail.com>
2018-02-07 21:51:02 -0400

    nav

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Proyecto_encuesta_php</title>
+	<link rel="stylesheet" type="text/css" href="css/styleprincipal.css">
+</head>
+
+<header>
+	
+<nav id="menu">
+		<ul>
+			<li><a href="index.php" id="logo">&nbsp;</a></li>
+			<li><a href="index.php" id="enlaces" class="inicio">Inicio</a></li>
+			<li><a href="index.php" id="enlaces">Encuestas</a></li>
+			<li><a href="index.php" id="enlaces">Responder</a></li>
+		</ul>
+		<a href="" id="pull">&nbsp;</a>
+	</nav>
+</header>
+
+	
